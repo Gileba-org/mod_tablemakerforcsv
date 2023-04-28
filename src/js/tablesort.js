bebbdd308aab5bd715912df167ea42b1ebf3c9e6
Gijs Lamon <info@gileba.be>
2023-04-28 10:59:53 +0200

    Fourth try

--- src/js/tablesort.js
+++ src/js/tablesort.js
@@ -61,7 +61,7 @@
 			if (!tbl) return;
 			var ths = tbl.getElementsByTagName("th");
 			var a;
-			for (var i = 0, th; (th = ths[i]) !== null; i++) {
+			for (var i = 0, th; ((th = ths[i]) !== null); i++) {
 				a = th.getElementsByTagName("a");
 				if (a.length) a[0].onkeydown = a[0].onclick = null;
 				th.onclick = th.onselectstart = th = a = null
